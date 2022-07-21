c2a49eca408863781523c547813ac21757c1158e
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2022-07-21 18:33:46 +0900

    _

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,68 @@
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
+// import typescript from "@rollup/plugin-typescript";
+// import dts from "rollup-plugin-dts";
+// import commonjs from "@rollup/plugin-commonjs";
+// import resolve from "@rollup/plugin-node-resolve";
+//
+// export default [
+//   {
+//     input: "src/index.ts",
+//     output: {
+//       file: "dist/index.js",
+//       format: "esm",
+//       sourcemap: true,
+//     },
+//     plugins: [
+//       resolve(),
+//       commonjs(),
+//       typescript({ tsconfig: "./tsconfig.json" }),
+//     ],
+//   },
+//   {
+//     input: "dist/types/index.d.ts",
+//     output: [{ file: "dist/index.d.ts", format: "esm" }],
+//     plugins: [dts()],
+//   },
+// ];
+
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+const pkg = require('./package.json');
 
 export default [
   {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.js",
-      format: "esm",
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: false,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: false,
+      },
     ],
+    plugins: [
+      commonjs({
+        include: ['node_modules/**'],
+      }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/__tests__/**'],
+      }),
+    ],
+    external: ['react', 'react-dom', '@emotion/styled', '@emotion/react'],
   },
   {
-    input: "dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: 'dist/cjs/types/index.d.ts',
+    output: [{ file: 'dist/cjs/index.d.ts', format: 'cjs' }],
+    plugins: [dts()],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/esm/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
 ];
