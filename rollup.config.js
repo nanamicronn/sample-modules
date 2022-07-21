1481c1cf5df3b3e00d5a8aa0cf5596f54a48dada
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2022-07-21 19:08:05 +0900

    _

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,3 @@
-// import typescript from "@rollup/plugin-typescript";
-// import dts from "rollup-plugin-dts";
-// import commonjs from "@rollup/plugin-commonjs";
-// import resolve from "@rollup/plugin-node-resolve";
-//
-// export default [
-//   {
-//     input: "src/index.ts",
-//     output: {
-//       file: "dist/index.js",
-//       format: "esm",
-//       sourcemap: true,
-//     },
-//     plugins: [
-//       resolve(),
-//       commonjs(),
-//       typescript({ tsconfig: "./tsconfig.json" }),
-//     ],
-//   },
-//   {
-//     input: "dist/types/index.d.ts",
-//     output: [{ file: "dist/index.d.ts", format: "esm" }],
-//     plugins: [dts()],
-//   },
-// ];
-
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
@@ -39,14 +13,11 @@ export default [
       },
     ],
     plugins: [
-      commonjs({
-        include: ['node_modules/**'],
-      }),
       typescript({
         tsconfig: './tsconfig.json',
       }),
     ],
-    external: ['react', 'react-dom', '@emotion/styled', '@emotion/react'],
+    // external: ['react', 'react-dom', '@emotion/styled', '@emotion/react'],
   },
   {
     input: "dist/types/index.d.ts",
