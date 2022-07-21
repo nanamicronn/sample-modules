7a49cb6e5ec27cba07e7a15feb4bb7847f1343ac
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2022-07-22 00:24:08 +0900

    _

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,31 @@
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: 'dist/index.js',
+        dir: 'dist',
         format: 'cjs',
         sourcemap: true,
+        exports: 'named',
       },
     ],
     plugins: [
-      commonjs({
-        include: ['node_modules/**'],
-      }),
-      typescript({
-        tsconfig: './tsconfig.json',
-      }),
+      // commonjs({
+      //   include: ['node_modules/**'],
+      // }),
+      typescript(),
+      terser()
     ],
     external: ['react', '@chakra-ui/react', '@emotion/styled', '@emotion/react'],
   },
-  {
-    input: "dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "cjs" }],
-    plugins: [dts()],
-  },
+  // {
+  //   input: "dist/types/index.d.ts",
+  //   output: [{ file: "dist/index.d.ts", format: "cjs" }],
+  //   plugins: [dts()],
+  // },
 ];
